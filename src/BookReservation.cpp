6f6af1baa6dfea26584330f4523ac841fa257c65
HyungJoon <itineris24@naver.com>
2023-09-30 15:48:27 -0700

    book reservation proto 1

--- src/BookReservation.cpp
+++ src/BookReservation.cpp
@@ -20,6 +20,7 @@ void BookReservationManagementSystem::indexBookToDB(const Book &book) {
     booksDB.push_back(book);
 }
 
+
 void BookReservationManagementSystem::enqueueReservation(const Patron &patron, const Book &book) {
     ReservationRecord rr(patron, book);
     pendingReservations.enqueue(rr);
@@ -30,11 +31,35 @@ void BookReservationManagementSystem::enqueueReservation(const ReservationRecord
     pendingReservations.enqueue(reservation);
 }
 
+
 ReservationRecord BookReservationManagementSystem::processReservation() {
+    int rotationCount = 0;
 
-    throw ReservationRecordUnavailable();
+    if (pendingReservations.isEmpty()) {
+        throw ReservationRecordUnavailable();
+    }
+    else {
+        while (!pendingReservations.isEmpty()) {
+            ReservationRecord currentReservation = pendingReservations.front();
+            bool reservedBook = false;
+
+            // Check if there are available copies for the requested book
+            for (auto &i : booksDB) {
+                if (currentReservation.bookISBN == i.ISBN && i.copies >= 1) {
+                    fulfilledReservations.push(currentReservation);
+                    pendingReservations.dequeue();
+                    i.copies--;
+                    return currentReservation;
+                }
+            }
+            pendingReservations.enqueue(currentReservation);
+            pendingReservations.dequeue();
+            rotationCount++;
+            if (rotationCount == pendingReservations.size())
+                throw ReservationRecordUnavailable();
+        }
+    }
 }
-
 
 
 
